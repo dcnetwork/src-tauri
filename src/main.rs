ff896fb30e8bd43f9c5d713c1214c52bb2b18592
Prakash <choudharyprakash066@gmail.com>
2022-05-24 23:46:09 +0530

    updates

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,38 @@
 //   all(not(debug_assertions), target_os = "windows"),
 //   windows_subsystem = "windows"
 // )]
-#[tauri::command]
-fn hello(name: &str) -> Result<String, String> {
-  // This is a very simplistic example but it shows how to return a Result
-  // and use it in the front-end.
-  if name.contains(' ') {
-    Err("Name should not contain spaces".to_string())
-  } else {
-    Ok(format!("Hello, {}", name))
-  }
+
+use node::stun::connect::*;
+use node::config::init::*;
+use serde::Serialize;
+use hex::*;
+
+#[derive(Serialize,Debug)]
+pub struct UserInfo{
+    address: String,
+    pubkey: Vec<u8>
 }
+
+
+#[tauri::command]
+async fn get_started(name: &str) -> Result<String, String> {
+
+    let node = DcNodeInit::new().await;
+
+    if false {
+        Err("Name should not contain spaces".to_string())
+    } else {
+        Ok(
+            encode_upper(node.address)
+        )
+    }
+
+}
+
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![hello])
+    .invoke_handler(tauri::generate_handler![get_started])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
