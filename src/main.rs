67f8ac0c78e0bdb4ee6ee8081d465c3eb1f25388
Prakash Choudhary <58355162+thelinuxpoint@users.noreply.github.com>
2022-06-11 22:46:31 +0530

    new command

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,23 @@ use hex::*;
 #[derive(Serialize,Debug)]
 pub struct UserInfo{
     address: String,
-    pubkey: Vec<u8>
+    pubkey: String
 }
 
+static mut CRED: Vec<String> = vec![];
 
 #[tauri::command]
-async fn get_started(name: &str) -> Result<String, String> {
+async fn get_started() -> Result<String, String> {
 
     let node = DcNodeInit::new().await;
-
+    
+    unsafe{
+        CRED.push(encode_upper(node.address.clone()));
+        CRED.push(encode_upper(node.pub_key_vec.clone()));
+    }
 
     if false {
-        Err("Name should not contain spaces".to_string())
+        Err("ERROROROROROR".to_string())
     } else {
         Ok(
             encode_upper(node.address)
@@ -30,10 +35,24 @@ async fn get_started(name: &str) -> Result<String, String> {
     }
 }
 
+#[tauri::command]
+async fn get_pub() -> Result<String, String> {
 
+    if false {
+        Err("ERROROROROROR".to_string())
+    } else {
+        Ok(
+            unsafe{
+                CRED[1].clone()
+            }
+        )
+    }
+}
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![get_started])
+
+    tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_started,get_pub])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
